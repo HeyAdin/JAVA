3d5083f07acd35d1bf7babd8492aa79a7919c015
HeyAdin <adinahmad003@gmail.com>
2023-07-02 21:27:54 +0530

    written code for finding maximum value in 2D array

--- LinearSearch/LSin2DArr.java
+++ LinearSearch/LSin2DArr.java
@@ -12,6 +12,7 @@ public class LSin2DArr{
         int target= 4;
         System.out.print("Element is at this Position : ");
         System.out.println(Arrays.toString(searchIn2D(arr, target)));
+        System.out.println("Maximum Value is : "+ maxIn2DArr(arr));
     }
 
     // function Linear Search in 2D Array
@@ -32,5 +33,28 @@ public class LSin2DArr{
             }
         }
         return ans;
+    }
+
+    // function to find maximum in 2D Array
+
+    static int maxIn2DArr(int[][] arr){
+        int maxVal= arr[0][0];
+        if(arr.length==0){
+            System.out.println("Null Array");
+            return -1;
+        }
+        
+        int[] arry=new int[2];
+        for(int row = 0; row<arr.length;row++){
+            for(int col =0; col<arr[row].length;col++){
+                if(maxVal<=arr[row][col]){
+                    maxVal=arr[row][col];
+                    arry[0]=row;
+                    arry[1]=col;
+                }
+            }
+        }
+        System.out.println("MAximum value index : "+Arrays.toString(arry));
+        return maxVal;
     }
 }
