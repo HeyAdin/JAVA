5cb552b69a1961227d6d4d235b9742a3e521be8b
HeyAdin <adinahmad003@gmail.com>
2023-07-08 21:05:14 +0530

    LeetCode Question where Lower Bound is implemented

---
+++ BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,25 @@
+public class SearchInsertPosition {
+    public static void main(String[] args){
+        int[] arr = {1, 2, 4, 5, 7, 9, 13, 15, 16, 21, 24, 25, 36};
+        int target = 12;
+        System.out.println(lowerBound(arr,target));
+    }
+
+    static int lowerBound(int[] arr, int target){
+        int low = 0;
+        int high = arr.length-1;
+        int ans = arr.length; //Hypothetical Index 
+
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid] >= target){
+                ans = mid;
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+}
