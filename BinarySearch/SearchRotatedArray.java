4756a5cc6e7ea9f8b4df90a6f49ae6db727609b3
HeyAdin <adinahmad003@gmail.com>
2023-07-10 00:32:36 +0530

    used short and optimized code

--- BinarySearch/SearchRotatedArray.java
+++ BinarySearch/SearchRotatedArray.java
@@ -1,26 +1,38 @@
 public class SearchRotatedArray {
-    public static void main(String[] args){
-        int[] arr = {23, 33, 34, 38, 39, 41, 45, 50, 51, 53, 58, 59, 62, 67, 70,73, 0, 1, 3, 4, 7, 8, 12, 13, 15, 16, 17, 19};
-        int target = 0;
-        System.out.println(searchInRotated(arr, target));
+    public static void main(String[] args) {
+        int[] arr = {1,3};
+        int target=3;
+        System.out.println(search(arr, target));
     }
 
-    static int searchInRotated(int[] nums, int target){
+    static int search(int[] nums,int target){
         int low = 0;
         int high = nums.length-1;
         while(low<=high){
-            int mid = low+(high-low)/2;
+            int mid=low+(high-low)/2;
             if(nums[mid]==target){
                 return mid;
             }
-            else if(nums[mid]>=target && target>=nums[high]){
-                high=mid-1;
+            else if(nums[mid]>=nums[low]){
+                if(target>= nums[low] && target<nums[mid]){
+                    high = mid -1;
+                }
+                else{
+                    low = mid+1;
+                }
             }
-            else if(nums[mid]<=target && target<=nums[low]){
-                low=mid+1;
+
+            else if(nums[mid]<=nums[low]){
+                if(target> nums[mid] && target<=nums[high]){
+                    low = mid +1;
+                }
+                else{
+                    high = mid-1;
+                }
             }
         }
         return -1;
     }
 
+    
 }
