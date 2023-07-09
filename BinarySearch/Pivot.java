41ebdcb8db6863164372df09463face406e0ad17
HeyAdin <adinahmad003@gmail.com>
2023-07-10 00:33:37 +0530

    descending technique

---
+++ BinarySearch/Pivot.java
@@ -0,0 +1,28 @@
+public class Pivot {
+    public static void main(String[] args) {
+        int[] arr= {1, 2, 4, 5, 7, 9, 13, 15, 16, 21, 24, 25, 36};
+        System.out.println(pivot(arr));
+    }
+
+    static int pivot(int[] nums){
+        int low =0; 
+        int high = nums.length-1;
+
+        if(nums[low]<nums[high]){
+            return low;
+        }
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(nums[mid]>nums[mid+1]){
+                return mid+1%(high+1);
+            }
+            else if(nums[mid]>=nums[low]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
