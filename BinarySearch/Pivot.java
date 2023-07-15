293730f06d0f1c6fcd728b09e816d66d67cbdc08
HeyAdin <adinahmad003@gmail.com>
2023-07-15 13:33:53 +0530

    next and prev method

--- BinarySearch/Pivot.java
+++ BinarySearch/Pivot.java
@@ -1,26 +1,28 @@
 public class Pivot {
     public static void main(String[] args) {
-        int[] arr= {1};
+        int[] arr = { 1 };
         System.out.println(pivot(arr));
     }
 
-    static int pivot(int[] nums){
-        int low =0; 
-        int high = nums.length-1;
+    static int pivot(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int n = nums.length;
+        while (low <= high) {
 
-        if(nums[low]<nums[high]){
-            return low;
-        }
-        while(low<=high){
-            int mid = low+(high-low)/2;
-            if(nums[mid]>nums[mid+1]){
-                return mid+1%(high+1);
+            if (nums[low] <= nums[high]) {
+                return nums[low];
             }
-            else if(nums[mid]>=nums[low]){
-                low = mid+1;
-            }
-            else{
-                high = mid-1;
+
+            int mid = low + (high - low) / 2;
+            int next = (mid + 1) % n;
+            int prev = (mid - 1 + n) % n;
+            if (nums[mid] <= nums[prev] && nums[mid] <= nums[next]) {
+                return nums[mid];
+            } else if (nums[low] <= nums[mid]) {
+                low = mid + 1;
+            } else if (nums[mid] <= nums[high]) {
+                high = mid - 1;
             }
         }
         return -1;
